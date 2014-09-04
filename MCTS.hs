261717ff1222fd5a30b5d3c73413962fa71a5a77
Vlad Firoiu <vladfi2@gmail.com>
2014-09-04 11:49:55 -0400

    Inconsistent state.

--- MCTS.hs
+++ MCTS.hs
@@ -6,35 +6,35 @@ import qualified Game
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
 
-import Data.Vector (Vector)
-import qualified Data.Vector as Vector
-
 import Data.Maybe (catMaybes)
 
-import Utils (listToIntMap, maximumByKey, iterateN, toVector, fromVector)
+import Utils (listToIntMap, iterateN)
 
-data Node s v = Node {
-    state     :: s,
-    value     :: v,
-    children  :: IntMap (Node s v)
+data Node a v = Node {
+--    state     :: s,
+    agent    :: a,
+    value    :: v,
+    children :: IntMap (Node a v)
   }
 
-agent :: Game a s => Node s v -> a
-agent node = Game.agent $ state node
+--agent :: Game a s => Node s v -> a
+--agent node = Game.agent $ state node
 
 listChildren = IntMap.elems . children
 getChild node n = (children node) IntMap.! n
 
+--bestChild :: Ord v 
 --bestChild node = maximumByKey (\child -> (getValue child) (agent node)) (listChildren node)
 
 initNode heuristic state =
   Node {
-    state = state,
+--    state = state,
+    agent = Game.agent state,
     value = heuristic state,
     children = listToIntMap $ map (initNode heuristic) (Game.actions state)
   }
 
-data Strategy v = Strategy {
+data Strategy a v = Strategy {
   pick        :: IntMap v -> Int,
   incorporate :: IntMap v -> Int -> v
 }
@@ -42,10 +42,14 @@ data Strategy v = Strategy {
 explore :: Game a s => Strategy v -> Node s v -> Node s v
 explore strategy node
   | Game.terminal (state node) = node
-  | otherwise = let 
-      index = (pick strategy) $ IntMap.map value (children node)
-      explored = explore strategy $ (children node) IntMap.! index
-      newChildren = IntMap.insert index explored (children node)
+  | otherwise = let
+      oldChildren = (children node)
+      index = (pick strategy) $ IntMap.map value oldChildren
+      explored = explore strategy $ oldChildren IntMap.! index
+      newChildren = IntMap.insert index explored oldChildren
       newValue = (incorporate strategy) (IntMap.map value newChildren) index
     in node {value = newValue, children = newChildren}
+
+mcts :: (Game a s) => Strategy v -> Int -> (s -> v) -> s -> Node s v
+mcts strategy n heuristic = (iterateN n $ explore strategy) . (initNode heuristic)
 
