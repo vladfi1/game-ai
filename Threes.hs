4924dac2b5bde098e8392d4b8a74ee07c9ec96cf
Evan Thomas <evanrthom@gmail.com>
2014-11-11 15:49:19 -0500

    initial commit

---
+++ Threes.hs
@@ -0,0 +1,10 @@
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
+
+import Graphics.UI.Threepenny.JQuery
+
+main :: IO ()
+main = startGUI defaultConfig setup
+
+setup :: Window -> UI ()
+setup rootWindow = undefined
