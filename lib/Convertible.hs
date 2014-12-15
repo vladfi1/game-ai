c2434f4712c8e774cdd15d28c1ad337ef4fab567
vladfi1 <vladfi2@gmail.com>
2014-12-15 13:51:26 -0500

    A few more Convertibles.

--- lib/Convertible.hs
+++ lib/Convertible.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverlappingInstances #-}
+--{-# LANGUAGE IncoherentInstances #-}
 
 module Convertible where
 
@@ -16,6 +17,12 @@ import qualified Data.BitVector as BV
 
 class Convertible a b where
   convert :: a -> b
+
+instance Convertible a a where
+  convert = id
+
+instance (Convertible a1 b1, Convertible a2 b2) => Convertible (a1, a2) (b1, b2) where
+  convert (a1, a2) = (convert a1, convert a2)
 
 instance (Monad m) => Convertible a (m a) where
   convert = return
@@ -41,6 +48,9 @@ instance (Storable a) => Convertible (Vector a) [a] where
 
 --instance (Convertible Bool b, Storable b) => Convertible BV (Vector b) where
 --  convert = convert . BV.toBits
+
+--lift :: (Convertible c a, Convertible b d) => (a -> b) -> (c -> d)
+--lift f = convert . f . convert
 
 class (Convertible a b, Convertible b a) => Isomorphic a b
 
