125a0bf0a25ad90bb775ac57f07b78808a6dd34a
vladfi1 <vladfi2@gmail.com>
2014-12-15 13:54:06 -0500

    join can be deduced from folding over a Monoid

--- lib/MonadJoin.hs
+++ lib/MonadJoin.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
-
---{-# LANGUAGE RankNTypes, TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE OverlappingInstances #-}
+--{-# LANGUAGE IncoherentInstances #-}
 
 module MonadJoin where
 
-import Prelude hiding (sequence)
-
 import Data.Functor.Compose
 import Data.Traversable
-
+import Data.Foldable
+import Data.Monoid
 import Control.Applicative
 
 class (Applicative m) => Monad' m where
@@ -31,11 +31,23 @@ instance (Monad' m, Monad' n, Traversable n) => Monad (Compose m n) where
   return = return'
   (>>=) = bind'
 
+-- necessary because we don't have polymorphic constraints
+class Monoid1 m where
+  mempty1 :: m a
+  mappend1 :: m a -> m a -> m a
+
+newtype WrapMonoid m a = WrapMonoid {unwrapMonoid :: m a}
+
+instance (Monoid1 m) => Monoid (WrapMonoid m a) where
+  mempty = WrapMonoid mempty1
+  mappend (WrapMonoid ma1) (WrapMonoid ma2) = WrapMonoid (ma1 `mappend1` ma2)
+
+instance (Monoid1 m, Foldable m, Applicative m) => Monad' m where
+  return' = pure
+  join' = unwrapMonoid . fold . (fmap WrapMonoid)
+
 -- standard
-instance Monad' [] where
-  return' = return
-  --{-# INLINABLE join' #-}
-  join' = (>>= id)
-  --join' = concat
-  
+instance Monoid1 [] where
+  mempty1 = mempty
+  mappend1 = mappend
 
